af7bcba80e8359de41661e508d107382b1a7e114
Mahbub2027 <alam201127@gmail.com>
2023-11-27 22:05:49 +0600

    booking modal added

--- src/pages/HomePages/Services/ServicesDetails.jsx
+++ src/pages/HomePages/Services/ServicesDetails.jsx
@@ -1,10 +1,51 @@
 import { useLoaderData } from "react-router-dom";
 import Navbar from "../../shared/Navbar";
 import Footer from "../../shared/Footer";
+import { useContext } from "react";
+import { AuthContext } from "../../../provider/AuthProvider";
 
 const ServicesDetails = () => {
+    const {user} = useContext(AuthContext);
     const details = useLoaderData();
     const { name, providerEmail, providerName, area, description, providerPhoto, price, photo } = details;
+    
+    
+    const handleBookServices = e=>{
+        const form = e.target;
+        const name = form.name.value;
+        const providerName = form.providerName.value;
+        const providerEmail = form.providerEmail.value;
+        const date = form.date.value;
+        const instruction = form.instruction.value;
+        const providerPhoto = form.providerPhoto.value;
+        const price = form.price.value;
+        const photo = form.photo.value;
+
+        const servicesDetails = {name, providerEmail, providerName, date, instruction, providerPhoto, price, photo};
+        console.log(servicesDetails);
+
+        fetch('http://localhost:5000/services', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json',
+            },
+            body: JSON.stringify(servicesDetails)
+        })
+        .then(res=> res.json())
+        .then(data=>{
+            console.log(data);
+            // Swal.fire({
+            //     title: 'Good job',
+            //     text: 'Service added successfully',
+            //     icon: 'success',
+            //     confirmButtonText: 'Ok'
+            //     })
+        })
+    }
+    
+    
+    
+    
     return (
         <div>
             <Navbar></Navbar>
@@ -29,14 +70,88 @@ const ServicesDetails = () => {
                 <div>
                     {/* Open the modal using document.getElementById('ID').showModal() method */}
                     <button className="btn" onClick={() => document.getElementById('my_modal_5').showModal()}>Book now</button>
-                    <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">Press ESC key or click the button below to close</p>
-                            <div className="modal-action">
+                    <dialog id="my_modal_5" className="modal ">
+                        <div className="modal-box w-10/12 max-w-4xl">
+                            <div className="">
                                 <form method="dialog">
+                                    {/* input field */}
+
+                                    <div className=" my-3">
+                                        <div className="  p-5">
+                                            <h2 className="text-4xl text-[#1c232e]  text-center font-bold my-8">Book Service</h2>
+                                            <form onSubmit={handleBookServices}>
+                                                    <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold" >Service name</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="text" name="name" value={name} placeholder="service name" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                    {/* <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold" >Service Provider Name</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="text" name="providerName" value={user.displayName} placeholder="Provider Name" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div> */}
+
+                                                    <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold">Service Provider email</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="email" name="providerEmail" value={user.email} placeholder="Email" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                    <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold">Services Taking date</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="text" name="date" placeholder="service taking date" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+
+                                                <div className="form-control w-full mb-5">
+                                                    <label className="label">
+                                                        <span className="label-text text-base font-bold">Service instruction</span>
+                                                    </label>
+                                                    <label className="input-group">
+                                                        <input type="text" name="instruction" placeholder=" instruction" className="input input-bordered w-full" />
+                                                    </label>
+                                                </div>
+
+                                                <div className="">
+                                                    <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold">Price</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="text" name="price" value={price} placeholder="price" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                <div className="">
+                                                    <div className="form-control w-full mb-5">
+                                                        <label className="label">
+                                                            <span className="label-text text-base font-bold">Photo</span>
+                                                        </label>
+                                                        <label className="input-group">
+                                                            <input type="text" name="photo" value={photo} placeholder="Photo URL" className="input input-bordered w-full" />
+                                                        </label>
+                                                    </div>
+                                                </div>
+
+                                                <input className="bg-orange-500 p-2 rounded-lg cursor-pointer w-full text-white  normal-case font-bold text-xl" type="submit" value="Purchase" />
+                                            </form>
+                                        </div>
+                                    </div>
+
                                     {/* if there is a button in form, it will close the modal */}
-                                    <button className="btn">Close</button>
+                                    {/* <button className="btn">Close</button> */}
                                 </form>
                             </div>
                         </div>
